69fa66558c6314092a492d48bda9c8f089004d9d
Aurimas M <aurimas113@gmail.com>
2021-10-21 13:45:16 +0300

    Added frontend to registration (sort of)
    Had to find a different way to do registration and login to be able to make it easier for frontend team to make things (html is enough, there is no need to use backend framework's features).
    
    The registration window is the one Pablo made, although so far it's only an universal registration (aka, not for vendors nor for customers), and as a result I removed some of registration sections (which are not yet in the system anyway)
    
    Also added the logo you can zoom into (which was made by Gustas) in the home page.

--- PinGoods/main/urls.py
+++ PinGoods/main/urls.py
@@ -5,8 +5,9 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login"),
-    path("logout", views.logout_request, name= "logout"),
+    path("", views.home_page, name="homepage"),
+    path("home", views.home_page, name="homepage"),
+    path("register", views.register_page, name="register"),
+    path("login", views.login_page, name="login"),
+    path("logout", views.logout_page, name= "logout"),
 ]
--- PinGoods/main/views.py
+++ PinGoods/main/views.py
@@ -1,43 +1,40 @@
-from django.shortcuts import  render, redirect
-from .forms import NewUserForm
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 
-def homepage(request):
-	return render(request=request, template_name='main/home.html')
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
-def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request=request, template_name="main/register.html", context={"register_form":form})
+        if user is not None:
+            login(request, user)
+            return redirect('main:home')
+        else:
+            messages.info(request, 'Try again! username or password is incorrect')
 
-def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:homepage")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="main/login.html", context={"login_form":form})
+    context = {}
+    return render(request, 'main/login.html', context)
 
-def logout_request(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("main:homepage")
+def logout_page(request):
+    logout(request)
+    return redirect('main:login')
+
+def home_page(request):
+    return render(request, 'main/home.html')
+
+def register_page(request):
+    if request.method != 'POST':
+        form = CustomUserForm()
+    else:
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:login')
+
+    context = {'form': form}
+
+    return render(request, 'main/register.html', context)
