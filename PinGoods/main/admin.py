eece8a4be27c8573c98596e35845b3fe5c97a613
Aurimas M <aurimas113@gmail.com>
2021-10-27 17:18:54 +0300

    Attempted to extend user profile
    Tried to add phone, address and email to registration, but didn't quite work out. Nevertheless, the profile has been extended. (since in django it's not the best idea to completely replace the user, but rather add some kind of extension via proxy to the user.)

--- PinGoods/main/admin.py
+++ PinGoods/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import User
+from main.models import Profile
 
-# Register your models here.
+admin.site.register(Profile)
