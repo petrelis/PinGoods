c4b9e74dc294f85047ec206b453674488b16c12b
Aurimas M <aurimas113@gmail.com>
2021-11-18 10:11:21 +0200

    Prepared for pull request (I hope?)

--- src/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.contrib.auth.models import User
-from main.models import Profile
-
-admin.site.register(Profile)
