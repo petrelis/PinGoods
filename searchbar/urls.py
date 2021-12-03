5f315b50197c39157a49385b51b73c9d8fada394
chiarasatta <chiara.satta@outlook.it>
2021-12-03 10:36:41 +0200

    add __icontains
    add __icontains in def

--- searchbar/urls.py
+++ searchbar/urls.py
@@ -1,7 +1,13 @@
+from django urls import path
+from . import views
+
 urlpatterns= [
     path ('', views.home, name='home' ),
     path('createpost/', views.createpost, name='home'),
     path ('uploadpost/', views.uploadpost, name='uploadpost'),
     path ('searchbar/', views.searchbar, name='searchbar'),
     path ('<slug>', views.details, name='details'),
+    path (' ', views.PostIndexViews.as_view(), name='post-list'),
+    path ('detail/ <int:pk>', views.PostIndexViews.as_view(), name 'post_detail'),
+    path ('searchbar/', views.BlogSearchView.as_view(), name ='searchbar')
 ]
