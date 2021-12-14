ba73d4a871a9a8bc6cc83a459d424282a1ceccb2
chiarasatta <chiara.satta@outlook.it>
2021-12-14 20:37:08 +0200

    Update views.py

--- searchbar/views.py
+++ searchbar/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.views.generic import ListView, DetailView
 
-class BlogSearchView(ListView):
+'''class BlogSearchView(ListView):
     model= Blog
     template_name ='home.html'
     context_object_name = 'posts'
@@ -10,10 +10,10 @@ class BlogSearchView(ListView):
   def get_queryset(self):
       query= self.request.GET.get('search')
       return Blog.objects.filter(title__icontains=query).order_by('-created_at')
+'''
 
-
-'''def searchbar (request):
+def searchbar (request):
     if request.method == 'GET' :
         search = request.GET.get('search')
         post= Post.objects.all ().filter(content_contains=result)
-        return render (request, 'searchbar.html, {'post': post}) '''
+        return render (request, 'searchbar.html, {'post': post}) 
